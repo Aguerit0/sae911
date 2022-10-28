97179ac18c651221a79aa526d815331c5529311d
Marcos Quinteros <beaterquinteros@gmail.com>
2022-10-28 13:04:40 -0300

    intentando conectar la base con novedades

--- conexion.php
+++ conexion.php
@@ -1,11 +1,11 @@
 <?php 
 	$servidor='localhost';
 	$usuario='root';
-	$clave='';
-	$bd='sae911';
+	$clave='41624421';
+	$bd='bdsae911';
 
 	$conexion=mysqli_connect($servidor,$usuario,$clave,$bd);
-	if (!conexion) {
+	if (!$conexion) {
 		echo "ERROR EN LA CONEXIÓN CON EL SERVIDOR";
 	}
  ?>
