f10f2a3bc840fcc2b71b6633dff07e27cbb42e3c
Marcos Quinteros <beaterquinteros@gmail.com>
2022-10-31 01:29:05 -0300

    previsualizar datos en el formulario de editar

--- verMas-novedadesGuardia.php
+++ verMas-novedadesGuardia.php
@@ -541,12 +541,13 @@
                       <div class="col-md-6">
                           <label for="inputDate" class="col-sm-2 col-form-label">Fecha</label>
                           <div class="col-sm-10">
-                            <input type="date" class="form-control" value="<?php echo $fecha;?>">
+                            <input type="date" class="form-control" value="<?php echo $fecha?>">
                           </div>
                         </div>
                         <div class="col-md-6">
                           <label for="inputState" class="form-label">Turno</label>
                           <select id="inputState" class="form-select" value="turno">
+                            <option value="<?php echo $turno?>"><?php echo $turno?></option>
                             <option >MATUTINO (06:00 - 14:00)</option>
                             <option>VESPERTINO (14:00 - 22:00)</option>
                             <option>NOCTURNO (22:00 - 06:00)</option>
@@ -554,55 +555,55 @@
                         </div>
                       <div class="col-md-6">
                         <label for="inputEmail5" class="form-label">Superior de Turno</label>
-                        <input type="text" class="form-control" id="inputEmail5" value="superior_de_turno">
+                        <input type="text" class="form-control" id="inputEmail5" value="<?php echo $superior_de_turno?>">
                       </div>
                       <div class="col-md-6">
                         <label for="inputtext5" class="form-label">Oficial Servicio</label>
-                        <input type="text" class="form-control" id="inputtext5" value="oficial_servicio">
+                        <input type="text" class="form-control" id="inputtext5" value="<?php echo $oficial_servicio?>">
                       </div>
                       <div class="col-md-6">
                         <label for="inputtext5" class="form-label">Cantidad de personal en guardia</label>
-                        <input type="text" class="form-control" id="inputtext5" value="personas_de_guardia">
+                        <input type="text" class="form-control" id="inputtext5" value="<?php echo $personas_de_guardia?>">
                       </div>
                       <div class="col-6">
                         <label for="inputAddress5" class="form-label">Motoristas</label>
-                        <input type="text" class="form-control" id="inputAddres5s" value="motoristas">
+                        <input type="text" class="form-control" id="inputAddres5s" value="<?php echo $motoristas?>">
                       </div>
                       <div class="col-6">
                           <label for="inputAddress5" class="form-label">Moviles en funcionamiento</label>
-                          <input type="text" class="form-control" id="inputAddres5s" value="mov_funcionamiento">
+                          <input type="text" class="form-control" id="inputAddres5s" value="<?php echo $mov_funcionamiento?>">
                         </div>
                       <div class="col-md-6">
                         <label for="inputtext5" class="form-label">Moviles fuera de servicio</label>
-                        <input type="text" class="form-control" id="inputtext5" value="mov_fuera_de_servicio">
+                        <input type="text" class="form-control" id="inputtext5" value="<?php echo $mov_fuera_de_servicio?>">
                       </div>
                       <div class="col-md-6">
                         <label for="inputtext5" class="form-label">Cantidad de detenidos Causa Federal</label>
-                        <input type="text" class="form-control" id="inputtext5" value="detenidos_causa_federal">
+                        <input type="text" class="form-control" id="inputtext5" value="<?php echo $detenidos_causa_federal?>">
                       </div>
                       <div class="col-md-6">
                         <label for="inputtext5" class="form-label">Cantidad de detenidos Justicia Ordinaria</label>
-                        <input type="text" class="form-control" id="inputtext5" value="detenidos_justicia_ordinaria">
+                        <input type="text" class="form-control" id="inputtext5" value="<?php echo $detenidos_justicia_ordinaria?>">
                       </div>
                       <div class="col-md-6">
                         <label for="inputtext5" class="form-label">Arrestados averiguacion del hecho</label>
-                        <input type="text" class="form-control" id="inputtext5" value="arres_averiguacion_de_hecho">
+                        <input type="text" class="form-control" id="inputtext5" value="<?php echo $arres_averiguacion_de_hecho?>">
                       </div>
                       <div class="col-md-6">
                         <label for="inputtext5" class="form-label">Cantidad de Aprehendidos</label>
-                        <input type="text" class="form-control" id="inputtext5" value="aprehendidos">
+                        <input type="text" class="form-control" id="inputtext5" value="<?php echo $aprehendidos?>">
                       </div>
                       <div class="col-md-6">
                         <label for="inputtext5" class="form-label">Arrestados averiguacion de activiades</label>
-                        <input type="text" class="form-control" id="inputtext5" value="arres_averiguacion_actividades">
+                        <input type="text" class="form-control" id="inputtext5" value="<?php echo $arres_averiguacion_actividades?>">
                       </div>
                       <div class="col-md-6">
                         <label for="inputtext5" class="form-label">Arrestados Inf. código de faltas</label>
-                        <input type="text" class="form-control" id="inputtext5" value="arres_info_codigo_de_faltas">
+                        <input type="text" class="form-control" id="inputtext5" value="<?php echo $arres_info_codigo_de_faltas?>">
                       </div>
                       <div class="col-md-6">
                         <label for="inputtext5" class="form-label">Demorados</label>
-                        <input type="text" class="form-control" id="inputtext5" value="demorados">
+                        <input type="text" class="form-control" id="inputtext5" value="<?php echo $demorados?>">
                       </div>
                       <div class="text-center">
                         <button type="submit" class="btn btn-primary float-end" value="guardarNovedad">Guardar</button>
