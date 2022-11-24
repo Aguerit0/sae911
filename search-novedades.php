4bc880bdae9b318bc3c4f788983cb7b632f31cb8
Esteban Agüero <cuentauniversidading@gmail.com>
2022-11-23 21:21:17 -0300

    search usu

--- search-novedades.php
+++ search-novedades.php
@@ -41,7 +41,7 @@ $where ";
 if ($_SESSION['rol']==1) {
     $sql2 = "SELECT * FROM  novedades_de_guardia n INNER JOIN comisarias c WHERE (n.eliminado<1) AND (n.idComisaria=c.idComisaria) AND (fecha LIKE '%$campo%' OR turno LIKE '%$campo%' OR superior_de_turno LIKE '%$campo%' OR oficial_servicio LIKE '%$campo%' OR nombre LIKE '%$campo%')";
 }else if ($_SESSION['rol']==0) {
-    $sql2 = "SELECT * FROM novedades_de_guardia n INNER JOIN comisarias c WHERE (n.idComisaria= c.idComisaria) AND (n.idComisaria = $id) AND (fecha LIKE '%$campo%' OR turno LIKE '%$campo%' OR superior_de_turno LIKE '%$campo%' OR oficial_servicio LIKE '%$campo%' OR nombre LIKE '%$campo%')";
+    $sql2 = "SELECT * FROM novedades_de_guardia INNER JOIN comisarias WHERE novedades_de_guardia.idComisaria=$id AND comisarias.idComisaria=$id AND (fecha LIKE '%$campo%' OR turno LIKE '%$campo%' OR superior_de_turno LIKE '%$campo%' OR oficial_servicio LIKE '%$campo%' OR nombre LIKE '%$campo%')";
 }
 
 
@@ -55,17 +55,9 @@ $html = '';
 
 if ($num_rows > 0) {
     while ($row = $resultado->fetch_assoc()) {
-
-        /*$idComisaria = $row['idComisaria'];
-        $cons="SELECT nombre FROM comisarias WHERE idComisaria=$idComisaria ";//CAPAZ QUE SE ACTUALIZA EL ID DE EDITAR AQUÍ
-        $res=mysqli_query($conexion,$cons);
-        if ($fila = $res->fetch_assoc()) {
-            $nombreComis=$fila['nombre'];
-        }*/
         if (($row['eliminado']>=1)) {
             
         }else{
-         
         $html .= '<tr>';
         $html .= '<th scope="row">' . $row['nombre'] .'</td>';
         $html .= '<th scope="row">' . $row['fecha'] . '</td>';
@@ -76,7 +68,6 @@ if ($num_rows > 0) {
         $html .= '<td scope="row"><a class="btn btn-primary" href="novedades-ver-mas.php?id=' . $row['id'] .'">Ver más</a></td>';
         $html .= '</tr>';   
         }
-
     }
 } else {
     $html .= '<tr>';
