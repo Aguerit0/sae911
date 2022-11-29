b7a932a8bc22f3fd194072cc15e7de6d2e9bcbdd
Marcos Quinteros <beaterquinteros@gmail.com>
2022-11-29 12:44:16 -0300

    editados los input de editar novedades  de relevancia

--- template/botonera-novedadesDeRelevancia.php
+++ template/botonera-novedadesDeRelevancia.php
@@ -27,6 +27,7 @@
        $medida_tomada=$_POST['medida_tomada'];
        $tipo=$_POST['tipo'];
        $subtipo=$_POST['subtipo'];
+       $descripcion=$_POST['txtDescr_Lugar'];
        $eliminado = $_POST['eliminado'];
 
        //CONSULTA PARA ACTUALIZAR VALORES EN BASE DE DATOS
@@ -117,56 +118,92 @@
                     <!-- FORMULARIO PARA EDITAR NOVEDADES DE RELEVANCIA -->
                     <form class="row g-4 pt-3" method="POST" action="">
                       <div class="col-md-6">
-                          <label for="fecha_reg_tabla" class="col-sm-2 col-form-label">Fecha de registro</label>
-                          <div class="col-sm-10">
-                            <input type="text" class="form-control" id="fecha_reg_tabla" name="fecha_reg_tabla" value="<?php echo $fecha_reg_tabla?>">
-                          </div>
-                        </div>
-                        <div class="col-md-6">
-                          <label for="fecha_reg" class="col-sm-2 col-form-label">Fecha del Hecho</label>
-                          <div class="col-sm-10">
-                            <input type="text" class="form-control" id="fecha_reg" name="fecha_reg" value="<?php echo $fecha_reg?>">
-                          </div>
-                        </div>
-                        <div class="col-md-6">
-                        <label for="hora_reg" class="form-label">Hora del Hecho</label>
-                        <input type="text" class="form-control" id="hora_reg" name="hora_reg" value="<?php echo $hora_reg?>">
+                        <label for="fecha_reg" class="col-form-label">Fecha del Hecho</label>
+                        <input type="date" class="form-control" id="fecha_reg" name="fecha_reg" value="<?php echo $fecha_reg?>">
                       </div>
                       <div class="col-md-6">
-                        <label for="sindicados" class="form-label">Sindicatos</label>
-                        <input type="text" class="form-control" id="sindicados" name="sindicados" value="<?php echo $sindicatos?>">
+                        <label for="inputEmail5" class="form-label">Hora</label>
+                        <input type="text" id="txtHora" name="txtHora" class="form-control clockpicker" data-placement="left" data-align="top" data-autoclose="true" readonly="" value="<?php echo $hora_reg?>">
+                      </div>
+                      <div class="col-md-6">
+                        <label for="inputState" class="form-label">Tipo</label>
+                        <select required id="tipo" name="tipo" class="form-select">
+                          <option value="<?php echo $tipo?>"><?php echo $tipo?></option>
+
+                          <option value="SUSTRACCION DE MOTOCICLETA">SUSTRACCION DE MOTOCICLETA</option>
+                          <option value="SUSTRACCION DE AUTOMOVIL">SUSTRACCION DE AUTOMOVIL</option>
+                          <option value="ILICITO CONTRA LA PROPIEDAD">ILICITO CONTRA LA PROPIEDAD</option>
+                          <option value="ARREBATO">ARREBATO</option>
+                          <option value="ILICITO EN LA VIA PUBLICA">ILICITO EN LA VIA PUBLICA</option>
+                          <option value="DESORDEN">DESORDEN</option>
+                          <option value="ABUSO SEXUAL">ABUSO SEXUAL</option>
+                          <option value="ACOSO SEXUAL">ACOSO SEXUAL</option>
+                          <option value="AMENAZAS">AMENAZAS</option>
+                          <option value="ARMAS">ARMAS</option>
+                          <option value="EXHIBICIONES OBSENAS">EXHIBICIONES OBSENAS</option>
+                          <option value="VIOLENCIA FAMILIAR Y DE GENERO">VIOLENCIA FAMILIAR Y DE GENERO</option>
+
+                        </select>
+                      </div>
+                      <div class="col-md-6">
+                        <label for="inputState" class="form-label">Subtipo</label>
+                        <select required id="subtipo" name="subtipo" class="form-select" disabled>
+                          <option value="<?php echo $subtipo?>"><?php echo $subtipo?></option>
+                        </select>
+                      </div>
+                      <div class="col-md-6">
+                        <label for="inputEmail5" class="form-label">Descripcion del lugar</label>
+                        <input required type="text" value="<?php echo $descripcion?>" id="txtDescr_Lugar" name="txtDescr_Lugar" class="form-control">
+                      </div>
+                      <div class="col-md-6">
+                        <label for="sindicados" class="form-label">Sindicados (cantidad)</label>
+                        <input type="number" class="form-control" id="sindicados" name="sindicados" value="<?php echo $sindicatos?>">
                       </div>
                       <div class="col-md-6">
                         <label for="caracteristicas_hecho" class="form-label">Caracteristicas hecho</label>
                         <input type="text" class="form-control" id="caracteristicas_hecho" name="caracteristicas_hecho" value="<?php echo $caracteristicas_hecho?>">
                       </div>
-                      <div class="col-md-6">
-                        <label for="elemento_utilizado" class="form-label"> Elemento utilizado</label>
-                        <input type="text" class="form-control" id="elemento_utilizado" name="elemento_utilizado" value="<?php echo $elemento_utilizado?>">
-                      </div>
                       <div class="col-6">
-                          <label for="movil" class="form-label">Movil</label>
-                          <input type="text" class="form-control" id="movil" name="movil" value="<?php echo $movil?>">
-                        </div>
+                        <label for="movil" class="form-label">Movil que asistio al lugar</label>
+                        <input type="text" class="form-control" id="movil" name="movil" value="<?php echo $movil?>">
+                      </div>
                       <div class="col-md-6">
                         <label for="elemento_sustraido" class="form-label">Elemento sustraido</label>
                         <input type="text" class="form-control" id="elemento_sustraido" name="elemento_sustraido" value="<?php echo $elemento_sustraido?>">
                       </div>
                       <div class="col-md-6">
-                        <label for="hecho_consumado" class="form-label">Hecho consumado</label>
-                        <input type="text" class="form-control" id="hecho_consumado" name="hecho_consumado" value="<?php echo $hecho_consumado?>">
+                        <label for="inputState" class="form-label">Hecho consumado o intento </label>
+                        <select required id="inputState" id="Hecho_Con_Int" name="Hecho_Con_Int" class="form-select">
+                            <option value="<?php echo $hecho_consumado?>"><?php echo $hecho_consumado?></option>
+                            <option value="Consumado">Consumado</option>
+                            <option value="Intento">Intento</option>
+                        </select>
                       </div>
                       <div class="col-md-6">
-                        <label for="tipo_motocicleta" class="form-label">Tipo motocicleta</label>
-                        <input type="text" class="form-control" id="tipo_motocicleta" name="tipo_motocicleta" value="<?php echo $tipo_motocicleta?>">
+                        <label for="inputState" class="form-label">Elemento utilizado (Moto o Pie)</label>
+                        <select required id="ElementoUtilizado" name="ElementoUtilizado" class="form-select">
+                          <option value="<?php echo $elemento_utilizado?>"><?php echo $elemento_utilizado?></option>
+                          <option value="Motocicleta">Motocicleta</option>
+                          <option value="Pie">Pie</option>            
+                        </select>
+                      </div>
+                      <div class="col-md-6">
+                        <label for="inputState" class="form-label">Tipo de motocicleta utilizada </label>
+                        <select required id="TipoMotocicleta" name="TipoMotocicleta" class="form-select" disabled>
+                          <option value="<?php echo $tipo_motocicleta?>"><?php echo $tipo_motocicleta?></option>
+                        </select>
                       </div>
                       <div class="col-md-6">
                         <label for="color" class="form-label">Color </label>
                         <input type="text" class="form-control" id="color" name="color"  value="<?php echo $color?>">
                       </div>
                       <div class="col-md-6">
-                        <label for="adelanto_circulacion" class="form-label">Adelanto circulacion</label>
-                        <input type="text" class="form-control" id="adelanto_circulacion" name="adelanto_circulacion" value="<?php echo $adelanto_circulacion?>">
+                        <label for="inputState" class="form-label">Emitio adelanto de circular</label>
+                        <select required id="inputState" id="EmitioAdelanto" name="EmitioAdelanto" class="form-select">
+                            <option value="<?php echo $adelanto_circulacion?>"><?php echo $adelanto_circulacion?></option>
+                            <option value="Si">Si</option>
+                            <option value="No">No</option>
+                        </select>
                       </div>
                       <div class="col-md-6">
                         <label for="damnificado" class="form-label">Damnificado</label>
@@ -174,31 +211,58 @@
                       </div>
                       <div class="col-md-6">
                         <label for="edad" class="form-label">Edad</label>
-                        <input type="text" class="form-control" id="edad" name="edad" value="<?php echo $edad?>">
+                        <input type="number" class="form-control" id="edad" name="edad" value="<?php echo $edad?>">
                       </div>
                       <div class="col-md-6">
-                        <label for="sexo" class="form-label">Sexo</label>
-                        <input type="text" class="form-control" id="sexo" name="sexo" value="<?php echo $sexo?>">
+                        <label for="inputState" class="form-label">Genero</label>
+                        <select required id="inputState" id="Sexo" name="Sexo" class="form-select">
+                            <option value="<?php echo $sexo?>"><?php echo $sexo?></option>
+                            <option value="Masculino">Masculino</option>
+                            <option value="Femenino">Femenino</option>
+                            <option value="No Binario">No Binario</option>
+                        </select>
+                      </div>
+                      <div class="col-md-6">
+                        <label for="inputState" class="form-label">Denuncia </label>
+                        <select required id="Denuncia" name="Denuncia" class="form-select">
+                            <option value="<?php echo $denuncia?>"><?php echo $denuncia?></option>
+                            <option value="Si">Si</option>
+                            <option value="No">No</option>
+                        </select>
                       </div>
                       <div class="col-md-6">
                         <label for="denunciante" class="form-label">Denunciante</label>
                         <input type="text" class="form-control" id="denunciante" name="denunciante" value="<?php echo $denunciante?>">
                       </div>
                       <div class="col-md-6">
-                        <label for="unidad_judicial" class="form-label">Unidad jucial</label>
-                        <input type="text" class="form-control" id="unidad_judicial" name="unidad_judicial" value="<?php echo $unidad_judicial?>">
+                        <label for="inputState" class="form-label">Unidad judicial </label>
+                        <select required id="UnidadJudicial" name="UnidadJudicial" class="form-select">
+                          <option value="<?php echo $unidad_judicial?>"><?php echo $unidad_judicial?></option>
+                        </select>
                       </div>
                       <div class="col-md-6">
-                        <label for="comision_personal" class="form-label">Comision personal</label>
-                        <input type="text" class="form-control" id="comision_personal" name="comision_personal" value="<?php echo $comision_personal?>">
+                        <label for="inputState" class="form-label">Comision de personal policial en la investigacion</label>
+                        <select required id="inputState" id="ComisionPolicialInvestigacion" name="ComisionPolicialInvestigacion" class="form-select">
+                            <option value="<?php echo $comision_personal?>"><?php echo $comision_personal?></option>
+                            <option value="Si">Si</option>
+                            <option value="No">No</option>
+                        </select>
                       </div>
                       <div class="col-md-6">
-                        <label for="tipo" class="form-label">Tipo</label>
-                        <input type="text" class="form-control" id="tipo" name="tipo" value="<?php echo $tipo?>">
-                      </div>
-                      <div class="col-md-6">
-                        <label for="subtipo" class="form-label">Sub Tipo</label>
-                        <input type="text" class="form-control" id="subtipo" name="subtipo" value="<?php echo $subtipo?>">
+                        <label for="inputState" class="form-label">Medida tomada </label>
+                        <select required id="inputState" id="MedidaTomada" name="MedidaTomada" class="form-select">
+                            <option value="<?php echo $medida_tomada?>"><?php echo $medida_tomada?></option>
+                            <option value="Fiscalia de instroduccion">Fiscalia de instroduccion</option>
+                            <option value="Demora">Demora</option>
+                            <option value="A.A.A">A.A.A</option>
+                            <option value="A.I.C.F">A.I.C.F</option>
+                            <option value="Aprehension">Aprehension</option>
+                            <option value="A.A echo">A.A Hecho</option>
+                            <option value="Detencion">Detencion</option>
+                            <option value="Secuestros">Secuestros</option>
+                            <option value="Registros">Registros</option>
+                            <option value="Allanamiento">Allanamiento</option>
+                        </select>
                       </div>
                      
                       <div class="text-center">
@@ -216,3 +280,12 @@
             </div>
           </div>
         </div>
+        <!-- Script de reloj -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="clockpicker.js"></script>
+        <script type="text/javascript">$('.clockpicker').clockpicker();</script>
+
+        <!-- Script de select -->
+        <script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script><script  src="novedades-relevancia-agregar.js"></script>
